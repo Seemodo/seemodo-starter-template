ce76dd671cfb09b25bcbfdeae8a3808445f568e0
Chris Feldmeier <chris@feldmeier-technology.com>
2026-02-23 01:52:14 +0700

    Add strictPort option in vite.config.ts to enforce port usage
    - Set strictPort to true to ensure the server fails if the specified port is in use, preventing automatic fallback to other ports.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
+    strictPort: true, // fail if port in use (e.g. after restart in Seemodo/[ctrl]) instead of switching to 5174, 5175
     allowedHosts: true as const,
     hmr: {
       overlay: false,
